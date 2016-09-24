37c18e1ede23d82dab583d2224690d3acda623f5
献隆 <xianlong.mxl@alibaba-inc.com>
2016-09-24 14:08:02 +0800

    javadoc

---
+++ codelist/Java/06javadoc/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+* The HelloWorld Class
+*
+* @author  xianlong.mxl
+* @version 1.0
+* @since   2016-09-24 
+*/
+public class HelloWorld {
+    public String content = "Hello World!";
+    public void Hello(){
+    	System.out.println(content);
+    }
+}
--- codelist/Java/06javadoc/Test.java
+++ codelist/Java/06javadoc/Test.java
@@ -19,6 +19,7 @@ public class Test {
      * This method is used to add two integers. This is
      * a the simplest form of a class method, just to
      * show the usage of various javadoc Tags.
+     * @author  xianlong.mxl
      * @param numA This is the first paramter to addNum method
      * @param numB  This is the second parameter to addNum method
      * @return int This returns sum of numA and numB.
